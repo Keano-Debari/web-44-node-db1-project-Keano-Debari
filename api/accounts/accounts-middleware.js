c62eecf9f00172fbb084f373f624bab2f7f5696a
unknown <Keano@Firelink.Firelink>
2021-08-09 14:43:52 -0700

    mvp

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,11 +1,48 @@
+const Account = require('./accounts-model');
+
 exports.checkAccountPayload = (req, res, next) => {
   // DO YOUR MAGIC
+  const {title, contents} = req.body
+  if(title && contents) {
+    next()
+  } else {
+    res.status(400).json("title and contents required")
+  }
 }
 
 exports.checkAccountNameUnique = (req, res, next) => {
   // DO YOUR MAGIC
+  let newName = req.body.name;
+  newName = newName.trim().toLowerCase();
+
+  Account.getAll()
+    .then ((accounts) => {
+      const existingAccount = accounts.find((account) => {
+        return account.name.trim().toLowerCase() === newName;
+      })
+
+      if (existingAccount) {
+        return res.status(400).json({ message: "that account name is taken" });
+      } else {
+        next();
+      }
+    })
+    .catch((err) => {
+      next(err);
+    });
 }
 
-exports.checkAccountId = (req, res, next) => {
+exports.checkAccountId = async (req, res, next) => {
   // DO YOUR MAGIC
+  try{
+    const account = await Account.getById(req.params.id)
+    if(account){
+      req.account = account
+      next()
+    } else {
+      res.status(404).json("Account not found")
+    }
+  } catch (err) {
+    next(err)
+  }
 }
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,84 @@
 const router = require('express').Router()
 
+const router = require('express').Router()
+
+const Account = require('./accounts-model');
+
+const imports = require('./accounts-middleware');
+const checkAccountId = imports.checkAccountId;
+const checkAccountPayload = imports.checkAccountPayload;
+const checkAccountNameUnique = imports.checkAccountNameUnique;
+
 router.get('/', (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    const accounts = await Account.getAll() 
+    console.log('accounts: ', accounts)
+    res.status(200).json(accounts)
+  } catch (err) {
+    next(err)
+  }
 })
 
 router.get('/:id', (req, res, next) => {
   // DO YOUR MAGIC
+  const {id} = req.params;
+  try {
+    const accountById = await Account.getById(id)
+    console.log('accountById: ', accountById);
+    res.status(200).json(accountById);
+  } catch (err) {
+    next(err)
+  }
 })
 
 router.post('/', (req, res, next) => {
   // DO YOUR MAGIC
+  const newAccount = req.body;
+  console.log('newAccount: ', newAccount);
+  try {
+    const postedAccount = await Account.create(newAccount)
+    console.log('postedAccount: ', postedAccount);
+    res.status(201).json(postedAccount);
+  } catch (err) {
+    console.log('Error in create: ', err)
+    next(err)
+  }
 })
 
 router.put('/:id', (req, res, next) => {
   // DO YOUR MAGIC
+  const id = req.params.id;
+  const changes = req.body;
+  try {
+    const updatedAccount = await Account.updateById(id, changes);
+    console.log('updatedAccount: ', updatedAccount);
+    res.status(200).json(updatedAccount);
+  } catch (err) {
+    console.log('Error in updateById: ', err)
+    next(err)
+  }
 });
 
 router.delete('/:id', (req, res, next) => {
   // DO YOUR MAGIC
+  const id = req.params.id;
+  try {
+    const deletedAccount = await Account.deleteById(id);
+    console.log('deletedAccount: ', deletedAccount);
+    res.status(200).json(deletedAccount);
+  } catch (err) {
+    console.log('Error in deleteById: ', err)
+    next(err)
+  }
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
   // DO YOUR MAGIC
+  const status = err.status || 500
+  res.status(status).json({
+    message: err.message,
+  })
 })
 
 module.exports = router;
